e0d55983a1519da34c8d47957dff5bd9686d56f4
Michael Osipov <michaelo@apache.org>
2019-01-28 23:21:52 +0100

    [MSHARED-797] Move current Build-Jdk manifest entry to Build-Jdk-Spec

--- src/main/java/org/apache/maven/archiver/MavenArchiver.java
+++ src/main/java/org/apache/maven/archiver/MavenArchiver.java
@@ -508,7 +508,7 @@ public class MavenArchiver
     private void addCustomEntries( Manifest m, Map<String, String> entries, ManifestConfiguration config )
         throws ManifestException
     {
-        addManifestAttribute( m, entries, "Build-Jdk", System.getProperty( "java.specification.version" ) );
+        addManifestAttribute( m, entries, "Build-Jdk-Spec", System.getProperty( "java.specification.version" ) );
 
         /*
          * TODO: rethink this, it wasn't working Artifact projectArtifact = project.getArtifact(); if (
